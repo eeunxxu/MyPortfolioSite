f7dec87efa4e3ff68fad6aaf7d8d8bf20e7c3ab2
eeunxxu <kes2450@naver.com>
2025-04-16 09:05:30 +0900

    ➕ [FE] add: TanstackQuery 및 Tailwind 설치

---
+++ my-portfolio/tailwind.config.ts
@@ -0,0 +1,26 @@
+// tailwind.config.ts
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}', // 사용하는 경우만
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+      },
+      fontFamily: {
+        sans: ['var(--font-geist-sans)'],
+        mono: ['var(--font-geist-mono)'],
+      },
+    },
+  },
+  darkMode: 'media', // 또는 "class"
+  plugins: [],
+};
+
+export default config;
